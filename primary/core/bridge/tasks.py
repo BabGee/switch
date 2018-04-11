fcd07fbaee95112afa9618be11f18d022b3bbe0f
root <root@APPS001.localdomain>
2018-04-12 00:07:39 +0300

    Updates

--- primary/core/bridge/tasks.py
+++ primary/core/bridge/tasks.py
@@ -72,8 +72,8 @@ class Wrappers:
 			 key not in ['ext_product_id','vpc_securehash','currency','amount'] and \
 			 'institution_id' not in key and key <> 'response' and key <> 'input' and key <> 'trigger' and \
 			 key not in ['send_minutes_period','send_hours_period','send_days_period','send_years_period']:
-				if count <= 30:
-					new_payload[str(k)[:50] ] = str(v)[:500]
+				if count <= 50:
+					new_payload[str(k)[:30] ] = str(v)[:500]
 				else:
 					break
 				count = count+1
