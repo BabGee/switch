2bcd31a614f4dab26f31012ad7eef6d3b04cc559
root <root@APPS001.localdomain>
2018-04-11 22:34:37 +0300

    Updates

--- secondary/erp/pos/tasks.py
+++ secondary/erp/pos/tasks.py
@@ -217,7 +217,7 @@ class Wrappers:
 			 'action_id' not in key and 'bridge__transaction_id' not in key and \
 			 'merchant_data' not in key and 'signedpares' not in key and \
 			 key <> 'gpid' and key <> 'sec' and \
-			 key not in ['ext_product_id','vpc_securehash','ext_inbound_id','currency','amount'] and \
+			 key not in ['ext_product_id','vpc_securehash','currency','amount'] and \
 			 'institution_id' not in key and key <> 'response' and key <> 'input':
 
 				if count <= 30:
