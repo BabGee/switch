bb714e62eb5292fb70b7cd0a6d90abcc2341b121
aritasamson <samson.arita@interintel.co.ke>
2018-04-29 09:58:29 +0300

    Update

--- secondary/channels/vcs/backend/page_string.py
+++ secondary/channels/vcs/backend/page_string.py
@@ -118,7 +118,8 @@ class PageString(ServiceCall, Wrappers):
 		payload.update(self.get_nav(navigator, node_info))
 
 
-		if navigator is not None and navigator.menu is not None and navigator.menu.submit == True and payload['input'] not in ['0','00']:
+		#if navigator is not None and navigator.menu is not None and navigator.menu.submit == True and payload['input'] not in ['0','00']:
+		if navigator is not None and navigator.menu is not None and navigator.menu.submit == True:
 			#payload = {}
 			#payload['chid'] = navigator.session.channel.id
 			#payload['ip_address'] = 'vcs'
