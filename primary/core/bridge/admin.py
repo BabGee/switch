56bcb7f9221afe74daeb208e37c7b1c391a81da9
aritasamson <samson.arita@interintel.co.ke>
2018-04-30 20:08:42 +0300

    Update

--- primary/core/bridge/admin.py
+++ primary/core/bridge/admin.py
@@ -50,7 +50,7 @@ class ServiceCommandAdmin(admin.ModelAdmin):
 		 'profile_status_list', 'channel_list',\
 		 'payment_method_list','trigger_list','gateway_list',)
 	        list_filter = ('service','node_system','gateway','access_level','channel','payment_method',)
-		search_fields = ('command_function','service__name','reverse_function','trigger__name',)
+		search_fields = ('command_function','service__name','reverse_function','trigger__name','details',)
 admin.site.register(ServiceCommand, ServiceCommandAdmin)
 
 class ServiceCutOffAdmin(admin.ModelAdmin):
