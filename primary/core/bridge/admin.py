15e1e4766d3a9575a0142c3f821a7136257f7e2a
aritasamson <samson.arita@interintel.co.ke>
2018-05-12 18:13:41 +0300

    register approval models admins

--- primary/core/bridge/admin.py
+++ primary/core/bridge/admin.py
@@ -116,6 +116,7 @@ class ActivityTransactionAdmin(admin.ModelAdmin):
                         'transaction_reference','currency','amount','charges','gateway','institution',\
                         'message','sends','ext_outbound_id',)
 admin.site.register(ActivityTransaction, ActivityTransactionAdmin)
+admin.site.register(Approval,ApprovalActivity,ApprovalActivityStatus)
 
 
 
