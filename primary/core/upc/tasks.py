7a21805c576db6c9097822477fb8079e825e00c2
aritasamson <samson.arita@interintel.co.ke>
2018-05-14 15:14:10 +0300

    Updates

--- primary/core/upc/tasks.py
+++ primary/core/upc/tasks.py
@@ -1825,7 +1825,7 @@ class System(Wrappers):
 				if user.profile.postal_code: payload['postal_code'] = user.profile.postal_code
 
 				if user.profile.dob: payload['dob'] = user.profile.dob.isoformat()
-
+				if user.profile.tax_pin: payload['tax_pin'] = user.profile.tax_pin
 
 				if 'msisdn' not in payload.keys() and session_gateway_profile[0].msisdn:
 					payload['msisdn'] = session_gateway_profile[0].msisdn.phone_number
@@ -1873,6 +1873,7 @@ class System(Wrappers):
 				if user.profile.address: payload['address'] = user.profile.address
 				if user.profile.postal_code: payload['postal_code'] = user.profile.postal_code
 				if user.profile.dob: payload['dob'] = user.profile.dob.isoformat()
+				if user.profile.tax_pin: payload['tax_pin'] = user.profile.tax_pin
 
 				if 'msisdn' not in payload.keys() and session_gateway_profile[0].msisdn:
 					payload['msisdn'] = session_gateway_profile[0].msisdn.phone_number
@@ -1929,6 +1930,7 @@ class System(Wrappers):
 				if user.profile.address: payload['address'] = user.profile.address
 				if user.profile.postal_code: payload['postal_code'] = user.profile.postal_code
 				if user.profile.dob: payload['dob'] = user.profile.dob.isoformat()
+				if user.profile.tax_pin: payload['tax_pin'] = user.profile.tax_pin
 
 				if 'msisdn' not in payload.keys() and session_gateway_profile[0].msisdn:
 					payload['msisdn'] = session_gateway_profile[0].msisdn.phone_number
