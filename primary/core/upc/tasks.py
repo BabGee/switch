62c1958d4ad0a240e2d57345f993953b0e2d2f95
aritasamson <samson.arita@interintel.co.ke>
2018-05-18 20:27:06 +0300

    Updates

--- primary/core/upc/tasks.py
+++ primary/core/upc/tasks.py
@@ -420,19 +420,19 @@ class System(Wrappers):
 			if self.validateEmail(email_msisdn):
 				lgr.info('With Email')
 				validate_gateway_profile = GatewayProfile.objects.get(user__email__iexact=email_msisdn, gateway=gateway_profile.gateway)
-				payload = device_validation(validate_gateway_profile, payload)
+				payload = device_activation(validate_gateway_profile, payload)
 
 			elif self.simple_get_msisdn(email_msisdn, payload):
 				lgr.info('With MSISDN')
 				validate_gateway_profile = GatewayProfile.objects.get(msisdn__phone_number=self.simple_get_msisdn(email_msisdn, payload), gateway=gateway_profile.gateway)
-				payload = device_validation(validate_gateway_profile, payload)
+				payload = device_activation(validate_gateway_profile, payload)
 
 			elif GatewayProfile.objects.filter(gateway=gateway_profile.gateway, user__username__iexact=email_msisdn).exists():
 				lgr.info('With Username')
 
 				lgr.info('GatewayProfile: %s' % gateway_profile)
 				validate_gateway_profile = GatewayProfile.objects.get(user__username__iexact=email_msisdn, gateway=gateway_profile.gateway)
-				payload = device_validation(validate_gateway_profile, payload)
+				payload = device_activation(validate_gateway_profile, payload)
 
 			else:
 				payload['response'] = 'MSISDN or Email Not Found'
