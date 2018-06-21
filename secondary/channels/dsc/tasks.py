f17f01198a6cb4693f19e46aa9ae01f915a9675e
aritasamson <samson.arita@interintel.co.ke>
2018-06-21 06:56:09 +0300

    Updates

--- secondary/channels/dsc/tasks.py
+++ secondary/channels/dsc/tasks.py
@@ -1017,7 +1017,8 @@ class Wrappers:
 			
 		#link_value = '%s%%%s%%%s' % (link_action, link_service, link_icon)
 
-		link_value = json.dumps(href)
+		#link_value = json.dumps(href)
+		link_value = href
 
 		#Final Case
 		if link_case_value and link_case_field:
