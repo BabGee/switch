557d18931d32bf068a75c54f0e3d6b017e38f31f
nyaundi brian <brian.nyaundi@interintel.co.ke>
2018-06-29 13:00:27 +0300

    distro get_trading_institution
    fix suppliers edit

--- secondary/erp/distro/tasks.py
+++ secondary/erp/distro/tasks.py
@@ -40,7 +40,8 @@ class System(Wrappers):
             session_gateway_profile = GatewayProfile.objects.get(id=payload['session_gateway_profile_id'])
 
             status = AgentStatus.objects.get(name='CREATED')
-            agent = Agent(profile=session_gateway_profile.user.profile,status=status, registrar=gateway_profile.user.profile)
+            agent = Agent(profile=session_gateway_profile.user.profile, status=status,
+                          registrar=gateway_profile.user.profile)
             agent.save()
             payload['response'] = 'Profile Registered'
             payload['response_status'] = '00'
@@ -67,29 +68,38 @@ class System(Wrappers):
 
     def get_trading_institution(self, payload, node_info):
         try:
-		gateway_profile = GatewayProfile.objects.get(id=payload['gateway_profile_id'])
-		institution = Institution.objects.get(id=payload['institution_id'])
-		agent = Agent.objects.get(profile=gateway_profile.user.profile)
-		trading_institution_list = TradingInstitution.objects.filter(institution=institution, agent=agent)
-		if trading_institution_list.exists():
-			trading_institution = trading_institution_list[0]
-		else:
-			trader_type = TraderType.objects.get(name=payload['trader_type'])
-			status = TradingInstitutionStatus.objects.get(name='CREATED')
-			trading_institution = TradingInstitution(trader_type=trader_type,
-						institution=institution,agent=agent,status=status)
-			trading_institution.save()
+            gateway_profile = GatewayProfile.objects.get(id=payload['gateway_profile_id'])
+            institution = Institution.objects.get(id=payload['institution_id'])
+            agent = Agent.objects.get(profile=gateway_profile.user.profile)
+            trading_institution_list = TradingInstitution.objects.filter(institution=institution, agent=agent)
+            if trading_institution_list.exists():
+                trading_institution = trading_institution_list[0]
+            else:
+                trader_type = TraderType.objects.get(name=payload['trader_type'])
+                status = TradingInstitutionStatus.objects.get(name='CREATED')
+                trading_institution = TradingInstitution(trader_type=trader_type,
+                                                         institution=institution, agent=agent, status=status)
+                trading_institution.save()
 
-		if 'supplier' in payload.keys():
-				supplier_list = TradingInstitution.objects.filter(institution__id__in=[a for a in payload['supplier'].split(',') if a])
-				for supplier in supplier_list: trading_institution.supplier.add(supplier)
+            if 'supplier' in payload.keys():
 
-		payload['trading_institution_id'] = trading_institution.id
-		payload['trading_institution_name'] = trading_institution.institution.name
+                suppliers = [int(a) for a in payload['supplier'].split(',') if a]
+                current_suppliers = trading_institution.supplier.values_list('pk', flat=True)
 
-		payload['enrollment_type_id'] = trading_institution.trader_type.enrollment_type.id
-		payload['response'] = 'Institution Captured'
-		payload['response_status'] = '00'
+                remove_suppliers = list(set(current_suppliers).difference(suppliers))
+                add_suppliers = list(set(suppliers).difference(current_suppliers))
+
+                trading_institution.supplier.add(*TradingInstitution.objects.filter(institution__id__in=add_suppliers))
+                trading_institution.supplier.remove(*TradingInstitution.objects.filter(
+                    institution__id__in=remove_suppliers
+                ))
+
+            payload['trading_institution_id'] = trading_institution.id
+            payload['trading_institution_name'] = trading_institution.institution.name
+
+            payload['enrollment_type_id'] = trading_institution.trader_type.enrollment_type.id
+            payload['response'] = 'Institution Captured'
+            payload['response_status'] = '00'
         except Exception, e:
             payload['response_status'] = '96'
             lgr.info("Error on get trading institution: %s" % e)
@@ -102,3 +112,4 @@ class Trade(System):
 
 class Registrations(System):
     pass
+
