38c593cb58198388c11ad8459604b86c1c3a4c82
brian nyaundi <brian.nyaundi@interintel.co.ke>
2018-06-29 09:57:42 +0300

    data.py industries_categories data fix data formating for DSC response

--- primary/core/administration/data.py
+++ primary/core/administration/data.py
@@ -34,65 +34,37 @@ from .models import *
 
 lgr = logging.getLogger('primary.core.administration')
 
-class List:
-    def industries_categories(self,payload,gateway_profile,profile_tz,data):
+
+class List(object):
+    def industries_categories(self, payload, gateway_profile, profile_tz, data):
+        max_id = 0
+        min_id = 0
+        ct = 0
+        push = {}
         r = []
-	iss = IndustrySection.objects.all()
-	'''
-	for i in iss:
-	    cl = {
-        	'name':i.isic_code,
-		'id':i.pk,
-	        'description':i.description,
-        	'divisions':[
-                     {
-                	'name':division.isic_code,
-			'id':division.pk,
-	                'description': division.description,
-        	        'groups':[
-                	    {
-                        	'name':group.isic_code,
-				'id':group.pk,
-	                        'description': group.description,
-        	                'classes':[
-                	            {
-                        	        'name': industry_class.isic_code,
-					'id':industry_class.pk,
-                                	'description': industry_class.description,
-	                            } for industry_class in group.industryclass_set.all()
-                        	]
-                    	    } for group in division.industrygroup_set.all()
-                	]
-            	    } for division in i.industrydivision_set.all()
-        	]
+        iss = IndustrySection.objects.all()
 
-    	    }
-	    r.append(cl)
-	#lgr.info(r)
-	params = dict(
-		data=r
-	)
+        c = [{"label": "name", "type": "string"}, {"label": "id", "type": "number"},
+             {"label": "description", "type": "string"}, {"label": "level", "type": "object"}]
 
-	'''
+        def _class(industryclass_set):
+            return [[industry_class.pk, industry_class.isic_code, industry_class.description] for industry_class in
+                    industryclass_set]
 
-	c = [{"label": "name", "type": "string"}, {"label": "id", "type": "number"},
-                          {"label": "description", "type": "string"}, {"label": "level", "type": "object"}]
+        def _group(industrygroup_set):
+            return [[group.pk, group.isic_code, group.description, _class(group.industryclass_set.all())] for group in
+                    industrygroup_set]
 
-	def _class(industryclass_set):
-		return [[industry_class.pk,industry_class.isic_code,industry_class.description] for industry_class in industryclass_set]
-	def _group(industrygroup_set): 
-		return [[group.pk,group.isic_code,group.description, _class(group.industryclass_set.all())] for group in industrygroup_set]
-	def _division(industrydivision_set):	
-		return [[division.pk,division.isic_code,division.description, _group(division.industrygroup_set.all())] for division in industrydivision_set]
+        def _division(industrydivision_set):
+            return [[division.pk, division.isic_code, division.description, _group(division.industrygroup_set.all())]
+                    for division in industrydivision_set]
 
-	for i in iss:
-	    r.append([i.pk,i.isic_code,i.description, _division(i.industrydivision_set.all())])
+        for i in iss:
+            r.append([i.pk, i.isic_code, i.description, _division(i.industrydivision_set.all())])
 
-	#lgr.info(r)
-	params = dict(
-		rows=r,
-		cols=c
-	)
-	return params
-
-
+        # lgr.info(r)
+        params = dict(
+            rows=r,
+            cols=c
+        )
+        return params, max_id, min_id, ct, push
