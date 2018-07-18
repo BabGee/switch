2414f703c437c34858f9413e4a00327d9ff72326
aritasamson <samson.arita@interintel.co.ke>
2018-07-18 06:38:42 +0300

    Update

--- secondary/erp/pos/tasks.py
+++ secondary/erp/pos/tasks.py
@@ -1297,8 +1297,8 @@ def process_paid_order():
 		processing = orig_order.filter(id__in=order).update(cart_processed=True, date_modified=timezone.now())
 		for od in order:
 			lgr.info('Order: %s' % od)
-			transaction.on_commit(lambda: order_background_service_call.delay(od))
-
+			#transaction.on_commit(lambda: order_background_service_call.delay(od))
+			order_background_service_call.delay(od)
 	except DatabaseError, e:
 		lgr.info('Transaction Rolled Back')
 		transaction.set_rollback(True)
