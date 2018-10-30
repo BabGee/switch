c1cc9a2853448e47c334fd8219fe223307a4745c
aritasamson <samson.arita@interintel.co.ke>
2018-10-30 11:53:57 +0300

    Updates

--- secondary/erp/pos/tasks.py
+++ secondary/erp/pos/tasks.py
@@ -17,7 +17,7 @@ from datetime import datetime
 from decimal import Decimal, ROUND_DOWN, ROUND_UP
 import base64, re
 from django.core.files import File
-from django.db.models import Count
+from django.db.models import Count, Sum
 from django.db import transaction
 
 from django.db.models import Q, F
@@ -232,6 +232,53 @@ class Wrappers:
 
 
 class System(Wrappers):
+	def settle_paid_orders(self, payload, node_info):
+		try:
+			
+			gateway_profile = GatewayProfile.objects.get(id=payload['gateway_profile_id'])
+			bill_manager_list = BillManager.objects.filter(order__status__name='PAID',order__cart_item__status__name='PAID')
+			if gateway_profile.institution:
+				bill_manager_list = bill_manager_list.filter(order__cart_item__product_item__institution=gateway_profile.institution)
+			elif 'institution_id' in payload.keys():
+				bill_manager_list = bill_manager_list.filter(order__cart_item__product_item__institution__id=payload['institution_id'])
+			else:
+				bill_manager_list = bill_manager_list.none()
+
+			lgr.info('Got Here')
+			if 'product_type_list' in payload.keys():
+				#product_type_list = '23,65,123,35,563,34,42'
+				bill_manager_list = bill_manager_list.filter(order__cart_item__product_item__product_type__id__in=[p for p in payload['product_type_list'].split(',') if p])
+
+			lgr.info('Got Here')
+			settled_amount = Decimal(0)
+			if bill_manager_list.exists():			
+				settled_amount = bill_manager_list.aggregate(Sum('balance_bf'))['balance_bf__sum']
+
+				lgr.info('Got Here')
+				for bill in bill_manager_list:
+					order = bill.order
+					order.status = OrderStatus.objects.get(name='SETTLED')
+					order.save()
+
+					lgr.info('Got Here')
+					order.cart_item.all().update(status=CartStatus.objects.get(name='SETTLED'))
+
+			lgr.info('Got Here')
+			payload['settled_amount'] = settled_amount
+
+			lgr.info('Got Here: 1')
+			payload["response_status"] = "00"
+
+			lgr.info('Got Here: 2')
+			payload['response'] = "Settled: %s" % settled_amount
+
+			lgr.info('Got Here: 3')
+		except Exception as e:
+			payload['response_status'] = '96'
+			lgr.info("error settling paid ordes: %s" % e)
+
+		return payload
+
 	def create_sale_charge(self, payload, node_info):
 		try:
 			gateway_profile = GatewayProfile.objects.get(id=payload['gateway_profile_id'])
