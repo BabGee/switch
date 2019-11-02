ce0c664001ef72110b36e662ae443448d8a42dc7
root <root@localhost.localdomain>
2019-11-02 09:31:23 +0300

    Updates

--- secondary/finance/paygate/tasks.py
+++ secondary/finance/paygate/tasks.py
@@ -781,7 +781,7 @@ class System(Wrappers):
 				float_type = float_type.filter(product_type=None)
 
 
-			if float_type.exists() and Decimal(payload['float_amount']) > Decimal(0):
+			if len(float_type) and Decimal(payload['float_amount']) > Decimal(0):
 				float_balance = FloatManager.objects.select_for_update(nowait=True).filter(float_type=float_type[0],gateway=gateway_profile.gateway).order_by('-date_created')
 
 
@@ -790,7 +790,7 @@ class System(Wrappers):
 				else:
 					float_balance = float_balance.filter(institution=None)
 
-				if float_balance.exists():
+				if len(float_balance):
 					charge = Decimal(0)
 					charge_list = FloatCharge.objects.filter(Q(float_type=float_type[0], min_amount__lte=Decimal(payload['float_amount']),\
 							max_amount__gt=Decimal(payload['float_amount']),credit=False),\
@@ -897,7 +897,7 @@ class System(Wrappers):
 			else:
 				float_type = float_type.filter(product_type=None)
 			#lgr.info('Float Type: %s' % float_type)
-			if float_type.exists() and Decimal(payload['float_amount']) > Decimal(0):
+			if len(float_type) and Decimal(payload['float_amount']) > Decimal(0):
 				float_balance = FloatManager.objects.select_for_update(of=('self',),nowait=True).filter(float_type=float_type[0],gateway=gateway_profile.gateway).order_by('-date_created').select_related()
 
 				if 'institution_id' in payload.keys():
@@ -906,7 +906,7 @@ class System(Wrappers):
 					float_balance = float_balance.filter(institution=None)
 				#lgr.info('Float Balance: %s' % float_balance)
 				#check float exists
-				if float_balance.exists() and Decimal(float_balance[0].balance_bf) >= Decimal(payload['float_amount']):
+				if len(float_balance) and Decimal(float_balance[0].balance_bf) >= Decimal(payload['float_amount']):
 
 					charge = Decimal(0)
 					charge_list = FloatCharge.objects.filter(Q(float_type=float_type[0], min_amount__lte=Decimal(payload['float_amount']),\
