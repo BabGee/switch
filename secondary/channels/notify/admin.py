73efd16880b24902bbe727c0eba2ed3577061eef
root <root@localhost.localdomain>
2020-01-08 08:03:25 +0300

    Updates

--- secondary/channels/notify/admin.py
+++ secondary/channels/notify/admin.py
@@ -96,7 +96,7 @@ class OutboundAdmin(admin.ModelAdmin):
 		list_display = ('id','contact','heading','message',\
 			'template','scheduled_send','state','sends',\
 			'ext_outbound_id','inst_notified','inst_num_tries','attachment_list',\
-			'recipient','response','pn','pn_ack')
+			'recipient','response','contact_group','pn','pn_ack')
 		list_filter = ('contact__product','state','contact__subscribed','contact__status','contact__product__notification__code__mno','contact__product__notification__code__institution','contact__product__notification__code__gateway',)
 		search_fields = ('id','recipient','contact__gateway_profile__msisdn__phone_number','contact__gateway_profile__user__email','contact__gateway_profile__user__username','message',)
 
