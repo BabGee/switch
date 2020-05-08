72a78152db7b92da1ee898cd448847ab200ef987
root <root@localhost.localdomain>
2020-05-08 05:33:46 +0300

    feat: VCS PageString to TextField
    Addition of the VCS PageString to TextField to allow for newline etc

--- secondary/channels/vcs/models.py
+++ secondary/channels/vcs/models.py
@@ -83,8 +83,8 @@ class InputVariable(models.Model):
 class Menu(models.Model):
 	date_modified  = models.DateTimeField(auto_now=True)
 	date_created = models.DateTimeField(auto_now_add=True)
-	page_string = models.CharField(max_length=320)
-	page_string_response = models.CharField(max_length=2048, blank=True, null=True, help_text='response_status%page_string%state|') 
+	page_string = models.TextField()
+	page_string_response = models.TextField(blank=True, null=True, help_text='response_status%page_string%state|') 
 	error_prefix = models.CharField(max_length=128, null=True, blank=True)
 	access_level = models.ManyToManyField(AccessLevel)
 	session_state = models.ForeignKey(SessionState, on_delete=models.CASCADE)
