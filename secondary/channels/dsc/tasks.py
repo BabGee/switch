45ca488060ee1f146541a73c8a00136169220e1b
root <root@localhost.localdomain>
2020-08-29 15:08:55 +0300

    Updates

--- secondary/channels/dsc/tasks.py
+++ secondary/channels/dsc/tasks.py
@@ -845,7 +845,7 @@ class Wrappers:
 			#lgr.info('Report List Count: %s' % len(report_list))
 			#lgr.info('Report End Date')
 			############################################VALUES BLOCK
-			args = values_date.keys()
+			args = values_data.keys()
 			if values not in [None,'']:
 				for i in values.split('|'):
 					try:k,v = i.split('%')
