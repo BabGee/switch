baf32d92700f696ed3c617e9e07525257f62f22d
Linux User <system@applications-0.applications.production.svc.cluster.local>
2020-11-09 03:24:03 +0000

    Updates

--- secondary/channels/notify/agents.py
+++ secondary/channels/notify/agents.py
@@ -96,18 +96,53 @@ async def _send_outbound_sms_messages_list(app):
 
                     lgr.info(f'3:Elapsed {elapsed}')
                     lgr.info('DF: %s' % df)
+                    df['batch'] = pd.to_numeric(df['batch'])
+                    df = df.dropna(axis='columns',how='all')
+                    cols = df.columns.tolist()
+                    #df.set_index(cols, inplace=True)
+                    #df = df.sort_index()
+                    cols.remove('kmp_recipients')
+                    grouped_df = df.groupby(cols)
+                    lgr.info('Grouped DF: %s' % grouped_df)
 
+                    tasks = []
+                    for name,group_df in grouped_df:
+                            batch_size = group_df['batch'].unique()[0]
+                            kmp_recipients = group_df['kmp_recipients'].unique().tolist()
+                            payload = dict()    
+                            for c in cols: payload[c] = str(group_df[c].unique()[0])
+                            lgr.info('MULTI: %s \n %s' % (group_df.shape,group_df.head()))
+                            if batch_size>1 and len(group_df.shape)>1 and group_df.shape[0]>1:
+                                    objs = kmp_recipients
+                                    lgr.info('Got Here (multi): %s' % objs)
+                                    start = 0
+                                    while True:
+                                            batch = list(islice(objs, start, start+batch_size))
+                                            start+=batch_size
+                                            if not batch: break
+                                            payload['kmp_recipients'] = batch
+                                            lgr.info(payload)
+                                            if is_bulk: tasks.append(bulk_send_outbound_batch_list.s(payload))
+                                            else: tasks.append(send_outbound_batch_list.s(payload))
+                            elif len(group_df.shape)>1 :
+                                    lgr.info('Got Here (list of singles): %s' % kmp_recipients)
+                                    for d in kmp_recipients:
+                                            payload['kmp_recipients'] = [d]       
+                                            lgr.info(payload)
+                                            if is_bulk: tasks.append(bulk_send_outbound_list.s(payload))
+                                            else: tasks.append(send_outbound_list.s(payload))
+                            else:
+                                    lgr.info('Got Here (single): %s' % kmp_recipients)
+                                    payload['kmp_recipients'] = kmp_recipients
+                                    lgr.info(payload)
+                                    if is_bulk: tasks.append(bulk_send_outbound_list.s(payload))
+                                    else: tasks.append(send_outbound_list.s(payload))
+
+                    lgr.info('Tasks: %s' % tasks)
                 #Control Speeds
                 await asyncio.sleep(0.10)
 
                 '''
-                        df['batch'] = pd.to_numeric(df['batch'])
-                        df = df.dropna(axis='columns',how='all')
-                        cols = df.columns.tolist()
-                        #df.set_index(cols, inplace=True)
-                        #df = df.sort_index()
-                        cols.remove('kmp_recipients')
-                        grouped_df = df.groupby(cols)
 
                         tasks = []
                         for name,group_df in grouped_df:
