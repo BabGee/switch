307a2af7fd080a8b205af2041ae2907eb715b64a
root <root@localhost.localdomain>
2021-01-10 11:09:46 +0300

    Updates

--- secondary/finance/paygate/tasks.py
+++ secondary/finance/paygate/tasks.py
@@ -337,41 +337,38 @@ class System(Wrappers):
 
 			if remittance_product.exists():
 				product = remittance_product.first()
-				######### Institution Incoming Service ###############
-				keyword = reference[:4] #Add Regex to Keyword in future
-				lgr.info('Keyword: %s' % keyword)
-				institution_incoming_service_list = InstitutionIncomingService.objects.filter(Q(remittance_product=product)|Q(remittance_product=None),\
-									Q(keyword__iexact=keyword)|Q(keyword__in=[''])|Q(keyword__iexact=reference)|Q(keyword__isnull=True))
 
-				if 'amount' in payload.keys() and payload['amount'] not in ["",None]:
-					amount = Decimal(payload['amount'])
-					institution_incoming_service_list = institution_incoming_service_list.filter(Q(min_amount__gte=amount)|Q(min_amount__isnull=True),\
-															Q(max_amount__lte=amount)|Q(max_amount__isnull=True))
-
-				purchase_order = PurchaseOrder.objects.filter(reference__iexact=reference, status__name='UNPAID', expiry__gte=timezone.now())
+				#Capture existing purchase order in order to skip institution incoming service
+				purchase_order = PurchaseOrder.objects.filter(reference__iexact=reference, status__name='UNPAID', expiry__gte=timezone.now(), gateway_profile__gateway=gateway_profile.gateway)
 				lgr.info('Order: %s' % purchase_order)
-				if institution_incoming_service_list.exists() and institution_incoming_service_list.count() == 1:
-					lgr.info('Keyword Found')
-					institution_incoming_service = institution_incoming_service_list[0]
-				elif institution_incoming_service_list.exists() and institution_incoming_service_list.count() > 1:
-					lgr.info('Multi Keyword Found') #Take the one with an empty keyword
-					institution_incoming_service_list = institution_incoming_service_list.filter(Q(keyword__in=[''])|Q(keyword__isnull=True))
-					institution_incoming_service = institution_incoming_service_list[0] if institution_incoming_service_list.exists() else None
 
-				#Execute Order Options
-				lgr.info('Institution Service: %s' % institution_incoming_service)
-				if institution_incoming_service and institution_incoming_service.process_order == True:
-					lgr.info('Process Order True')
-					if purchase_order.exists():pass #process_order=True - Only Process where order exists
-					else: institution_incoming_service = None
-				elif institution_incoming_service and institution_incoming_service.process_order == False:
-					lgr.info('Process Order False') #DO NOT PROCESS with incoming service if order exists in POS
-					if purchase_order.exists(): institution_incoming_service = None
-					else: pass #process_order=False - Only Process where order DOES NOT exist
-				else: pass #process all
+				if not purchase_order.exists():
+					######### Institution Incoming Service ###############
 
-				######### Institution Incoming Service ###############
+					#keyword = reference[:4] #Add regex to get Keyword in future
+					keyword = ''.join(re.findall(r'(^[A-Za-z]+)(?<=[\S])|(?<=[\d])', reference)).lower()  #Add Intent Classification Model to get Keyword/intent in future
+					if keyword:
+						lgr.info('Keyword: %s' % keyword)
+						institution_incoming_service_list = InstitutionIncomingService.objects.filter(Q(remittance_product=product)|Q(remittance_product=None),\
+											Q(keyword__iexact=keyword)|Q(keyword='')|Q(keyword__isnull=True))
 
+						if len(institution_incoming_service_list):
+							if 'amount' in payload.keys() and payload['amount'] not in ["",None]:
+								amount = Decimal(payload['amount'])
+								institution_incoming_service_list = institution_incoming_service_list.filter(Q(min_amount__gte=amount)|Q(min_amount__isnull=True),\
+																		Q(max_amount__lte=amount)|Q(max_amount__isnull=True))
+
+							if institution_incoming_service_list.count() == 1:
+								lgr.info('Keyword Found')
+								institution_incoming_service = institution_incoming_service_list.last()
+							else:
+								lgr.info('Multi Keyword Found') #Take the one with an empty keyword
+								_institution_incoming_service_list = institution_incoming_service_list.filter(Q(keyword__in=[''])|Q(keyword__isnull=True))
+								institution_incoming_service = _institution_incoming_service_list.first() if _institution_incoming_service_list.exists() else institution_incoming_service_list.last()
+
+							lgr.info('Institution Service: %s' % institution_incoming_service)
+
+					######### Institution Incoming Service ###############
 
 				# Capture remittance product institution
 
