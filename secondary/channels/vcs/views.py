656e767c421763c0f8a8c898f62b7141dcaf4ad6
root <root@localhost.localdomain>
2021-05-01 15:51:15 +0300

    Updates

--- secondary/channels/vcs/views.py
+++ secondary/channels/vcs/views.py
@@ -165,10 +165,13 @@ class VAS:
 
 			menuitems = menuitems.filter(menu=self.nav.menu)
 
-			if error_prefix:
+			if error_prefix and self.channel in ['USSD','IVR']:
 				message = error_prefix.split('|')
 				if len(message)>1 and message[1].strip()=='REPLACE': page_string = '%s' % (message[0])
 				else: page_string = '%s %s%s' % (error_prefix, self.payload['page_string'], get_menu_items(menuitems))
+			elif error_prefix:
+				message = error_prefix.split('|')
+				self.payload['error_prefix'] = message
 			else:
 				page_string = '%s%s' % (self.payload['page_string'], get_menu_items(menuitems))
 
