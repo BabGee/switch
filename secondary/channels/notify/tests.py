769f1cc86db6b761165d4c3f2e83b5dcf1b8e54f
root <root@localhost.localdomain>
2021-05-15 23:01:47 +0300

    Updates

--- secondary/channels/notify/tests.py
+++ secondary/channels/notify/tests.py
@@ -10,16 +10,16 @@ from .models import *
 
 class InboundTests(TestCase):
 
-    def test_inserts_to_inbound(self):
-        """
-        """
-        #time = timezone.now() + datetime.timedelta(days=30)
-        #future_question = Question(pub_date=time)
-        #self.assertIs(future_question.was_published_recently(), False)
-	contact = Contact.objects.get(id=2247)
-	#Append by adding
-	_recipient = ['254717103598']*3
-	obj_list = [Outbound(contact=contact, message='Test', scheduled_send=timezone.now(),state=OutBoundState.objects.get(name='CREATED'), recipient=r, sends=0) for r in _recipient]
-	if obj_list:
-		outbound_log = Outbound.objects.bulk_create(obj_list)
+	def test_inserts_to_inbound(self):
+		"""
+		"""
+		#time = timezone.now() + datetime.timedelta(days=30)
+		#future_question = Question(pub_date=time)
+		#self.assertIs(future_question.was_published_recently(), False)
+		contact = Contact.objects.get(id=2247)
+		#Append by adding
+		_recipient = ['254717103598']*3
+		obj_list = [Outbound(contact=contact, message='Test', scheduled_send=timezone.now(),state=OutBoundState.objects.get(name='CREATED'), recipient=r, sends=0) for r in _recipient]
+		if obj_list:
+			outbound_log = Outbound.objects.bulk_create(obj_list)
 
