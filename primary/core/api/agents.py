01f965338b04b650af139fdc859b35aa0b8854f9
root <root@localhost.localdomain>
2021-05-15 13:55:21 +0300

    Updates

--- primary/core/api/agents.py
+++ primary/core/api/agents.py
@@ -105,7 +105,7 @@ async def _interface(_requests):
 					    #response=json.loads(response.content)
 					)
 
-		await transformed_api_topic.send(transformed)   
+		await transformed_api_topic.send(value=transformed)   
 		lgr.info(f'Interface Request: {_request.service_name}')
 
 '''
