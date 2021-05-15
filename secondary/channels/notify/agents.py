6b43d3b9bb2f905576d493eae8af4f7fef705724
root <root@localhost.localdomain>
2021-05-15 22:58:48 +0300

    Updates

--- secondary/channels/notify/agents.py
+++ secondary/channels/notify/agents.py
@@ -90,13 +90,13 @@ async def _send_outbound_sms_messages_list(app):
 		lgr.info(f'0:Elapsed {elapsed}')
 		with transaction.atomic():
 			def outbound_query():        
-				return Outbound.objects.select_for_update(of=('self',)).filter(Q(contact__subscribed=True),Q(contact__product__notification__code__channel__name='SMS'),\
+				return Outbound.objects.select_for_update(of=('self',)).filter(Q(contact__subscribed=True),Q(contact__product__notification__code__channel__name='WHATSAPP API'),\
                                                 Q(Q(contact__product__trading_box=None)|Q(contact__product__trading_box__open_time__lte=timezone.localtime().time(),contact__product__trading_box__close_time__gte=timezone.localtime().time())),\
                                                 ~Q(recipient=None),~Q(recipient=''),~Q(contact__product__notification__endpoint__url=None),~Q(contact__product__notification__endpoint__url=''),\
                                                 Q(scheduled_send__lte=timezone.now(),state__name='CREATED',date_created__gte=timezone.now()-timezone.timedelta(hours=24))\
                                                 |Q(state__name="PROCESSING",date_modified__lte=timezone.now()-timezone.timedelta(minutes=20),date_created__gte=timezone.now()-timezone.timedelta(minutes=60))\
                                                 |Q(state__name="FAILED",date_modified__lte=timezone.now()-timezone.timedelta(minutes=20),date_created__gte=timezone.now()-timezone.timedelta(minutes=60)),\
-                                                Q(contact__status__name='ACTIVE',contact__product__is_bulk=is_bulk),Q(contact__product__notification__endpoint__name='TEST NOTIFICATION')).order_by('contact__product__priority').select_related('contact').all
+                                                Q(contact__status__name='ACTIVE',contact__product__is_bulk=is_bulk)).order_by('contact__product__priority').select_related('contact').all
 
 			#orig_outbound = await outbound_query()
 			orig_outbound = await sync_to_async(outbound_query, thread_sensitive=True)()
