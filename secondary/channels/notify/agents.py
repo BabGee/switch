d1d9eba3941ffaa7310208323392904c973d3925
root <root@localhost.localdomain>
2021-05-16 07:26:22 +0300

    Updates

--- secondary/channels/notify/agents.py
+++ secondary/channels/notify/agents.py
@@ -101,7 +101,6 @@ async def _send_outbound_sms_messages_list(app):
 
 			#orig_outbound = await outbound_query()
 			orig_outbound = await sync_to_async(outbound_query, thread_sensitive=True)()
-			'''
 
 			def outbound_query():        
 				return Outbound.objects.select_for_update(of=('self',)).filter(Q(contact__subscribed=True),Q(contact__product__notification__code__channel__name='WHATSAPP API'),\
@@ -111,6 +110,17 @@ async def _send_outbound_sms_messages_list(app):
                                                 |Q(state__name="PROCESSING",date_modified__lte=timezone.now()-timezone.timedelta(minutes=20),date_created__gte=timezone.now()-timezone.timedelta(minutes=60))\
                                                 |Q(state__name="FAILED",date_modified__lte=timezone.now()-timezone.timedelta(minutes=20),date_created__gte=timezone.now()-timezone.timedelta(minutes=60)),\
                                                 Q(contact__status__name='ACTIVE',contact__product__is_bulk=is_bulk)).order_by('contact__product__priority').select_related('contact','state','template').all
+			'''
+
+			def outbound_query():        
+				return Outbound.objects.select_for_update(of=('self',)).filter(Q(contact__subscribed=True),Q(contact__product__notification__code__channel__name='WHATSAPP API'),~Q(recipient=None),
+					Q(contact__status__name='ACTIVE',contact__product__is_bulk=is_bulk))\
+					.filter(Q(Q(contact__product__trading_box=None)|Q(contact__product__trading_box__open_time__lte=timezone.localtime().time(),
+					contact__product__trading_box__close_time__gte=timezone.localtime().time())))\
+					.filter(Q(scheduled_send__lte=timezone.now(),state__name='CREATED',date_created__gte=timezone.now()-timezone.timedelta(hours=24))\
+					|Q(state__name="PROCESSING",date_modified__lte=timezone.now()-timezone.timedelta(minutes=20),date_created__gte=timezone.now()-timezone.timedelta(minutes=60))\
+					|Q(state__name="FAILED",date_modified__lte=timezone.now()-timezone.timedelta(minutes=20),date_created__gte=timezone.now()-timezone.timedelta(minutes=60)))\
+					.order_by('contact__product__priority').select_related('contact','template','state').all
 
 			#orig_outbound = await outbound_query()
 			orig_outbound = await sync_to_async(outbound_query, thread_sensitive=True)()
