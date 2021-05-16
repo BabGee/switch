f996fe260c1f12b0a76c8677f1ecd2dc9570ed9b
root <root@localhost.localdomain>
2021-05-16 12:22:49 +0300

    Updates

--- secondary/channels/notify/agents.py
+++ secondary/channels/notify/agents.py
@@ -9,6 +9,7 @@ from django.db import transaction
 from .models import *
 from django.db.models import Q,F
 
+from itertools import islice
 import pandas as pd
 import numpy as np
 import time
@@ -48,39 +49,88 @@ HTTPConnection.debuglevel = 1
 s = time.perf_counter()
 
 
-class Greeting(faust.Record):
+
+class OutBoundMessage(faust.Record):
     from_name: str
     to_name: str
     count: float
     records: int
+{'product': '433', 'batch': '10', 'kmp_service_id': 'TEST', 'kmp_code': 'TEST', 'kmp_message': 'Test', 'kmp_spid': 'TEST', 'kmp_password': 'TEST', 'node_account_id': 'TEST', 'node_password': 'TEST', 'node_username': 'TEST', 'contact_info': '', 'node_url': 'integrator.apps.test.notification', 'kmp_recipients': ['254725765441', '254717103598']} 
+
+topic = app.topic('switch.channels.notify.whatsapp_message', value_type=Greeting)
 
 
-topic = app.topic('switch-hello-topic', value_type=Greeting)
+async def send_outbound_message(messages):
+	try:
 
-@app.agent(topic)
-async def hello(greetings):
-	async for greeting in greetings:
-		lgr.info(f'Hello from {greeting.from_name} to {greeting.to_name} | Count {greeting.count} | Records {greeting.records}')
+		'''
+		'id','recipient','contact__product__id','contact__product__notification__endpoint__batch','ext_outbound_id',\
+		'contact__product__notification__ext_service_id','contact__product__notification__code__code','message','contact__product__notification__endpoint__account_id',\
+		'contact__product__notification__endpoint__password','contact__product__notification__endpoint__username','contact__product__notification__endpoint__api_key',\
+		'contact__subscription_details','contact__linkid','contact__product__notification__endpoint__url'
+		'''
+		count = time.perf_counter() - s
+		elapsed = "{0:.2f}".format(count)
+		'''
+		df = pd.DataFrame({'kmp_recipients':messages[:,1], 'product':messages[:,2], 'batch':messages[:,3],'kmp_correlator':messages[:,4],'kmp_service_id':messages[:,5],'kmp_code':messages[:,6],\
+			'kmp_message':messages[:,7],'kmp_spid':messages[:,8],'kmp_password':messages[:,9],'node_account_id':messages[:,8],'node_password':messages[:,9],'node_username':messages[:,10],\
+			'node_api_key':messages[:,11],'contact_info':messages[:,12],'linkid':messages[:,13],'node_url':messages[:,14]})
+		'''
+		df = pd.DataFrame({'outbound_id':messages[:,0], 'recipient':messages[:,1], 'product_id':messages[:,2], 'batch':messages[:,3],'ext_outbound_id':messages[:,4],'ext_service_id':messages[:,5],'code':messages[:,6],\
+			'message':messages[:,7],'account_id':messages[:,8],'endpoint_password':messages[:,9],'endpoint_username':messages[:,10],\
+			'endpoint_api_key':messages[:,11],'subscription_details':messages[:,12],'linkid':messages[:,13],'endpoint_url':messages[:,14]})
 
-#@app.task
-#@app.timer(interval=0.25)
-@app.timer(interval=10)
-async def example_sender_task(app):
-    count = time.perf_counter() - s
-    elapsed = "{0:.2f}".format(count)
-    records = random.randint(1,1000) 
-    await hello.send(
-        value=Greeting(from_name='Switch Notify Task', to_name='you', count=float(elapsed), records=records),
-    )
-    count+=1
+		lgr.info(f'3:Elapsed {elapsed}')
+		lgr.info('DF: %s' % df)
+		df['batch'] = pd.to_numeric(df['batch'])
+		df = df.dropna(axis='columns',how='all')
+		cols = df.columns.tolist()
+		#df.set_index(cols, inplace=True)
+		#df = df.sort_index()
+		cols.remove('outbound_id')
+		cols.remove('recipient')
+		grouped_df = df.groupby(cols)
+		lgr.info('Grouped DF: %s' % grouped_df)
 
+
+		tasks = []
+		for name,group_df in grouped_df:
+			batch_size = group_df['batch'].unique()[0]
+			outbound_id_list = group_df['outbound_id'].tolist()
+			recipient_list = group_df['recipient'].tolist()
+			recipients = tuple(zip(outbound_id_list, recipient_list))
+			payload = dict()    
+			for c in cols: payload[c] = str(group_df[c].unique()[0])
+			lgr.info('MULTI: %s \n %s' % (group_df.shape,group_df.head()))
+			if batch_size>1 and len(group_df.shape)>1 and group_df.shape[0]>1:
+				objs = recipients
+				lgr.info('Got Here (multi): %s' % objs)
+				start = 0
+				while True:
+					batch = list(islice(objs, start, start+batch_size))
+					start+=batch_size
+					if not batch: break
+					payload['recipients'] = batch
+					lgr.info(payload)
+			elif len(group_df.shape)>1 :
+				lgr.info('Got Here (list of singles): %s' % recipients)
+				for d in recipients:
+					payload['recipients'] = [d]       
+					lgr.info(payload)
+			else:
+				lgr.info('Got Here (single): %s' % recipients)
+				payload['recipients'] = recipients
+				lgr.info(payload)
+			#Control Speeds
+			#await asyncio.sleep(0.10)
+
+	except Exception as e: lgr.error(f'Send Outbound Message Error: {e}')
 
 is_bulk = False
 limit_batch = 100
 
-#@app.task
+
 @app.timer(interval=10)
-#@transaction.atomic
 async def send_outbound_messages(app):
 	try:
 		lgr.info(f'{app}')
