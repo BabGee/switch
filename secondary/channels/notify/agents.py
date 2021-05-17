46a324f7daf3f78d78b3d9451c8233af7193203b
root <root@localhost.localdomain>
2021-05-18 02:53:13 +0300

    Updates

--- secondary/channels/notify/agents.py
+++ secondary/channels/notify/agents.py
@@ -2,7 +2,7 @@ import faust
 from faust.types import StreamT
 #from primary.core.async.faust import app
 from switch.faust import app
-import requests, json
+import requests, json, ast
 
 
 from django.db import transaction
@@ -115,15 +115,16 @@ async def send_outbound_message(messages):
 		lgr.info(f'3:Elapsed {elapsed()}')
 		lgr.info('DF: %s' % df)
 		df['batch'] = pd.to_numeric(df['batch'])
+
+		#if 'endpoint_request' in df.columns:
+		#df['endpoint_request'] = df['endpoint_request'].to_json(orient="records")
+
+		df['endpoint_request']= df['endpoint_request'].fillna({i: {} for i in df.index})
+		df['endpoint_request'] = df['endpoint_request'].apply(ast.literal_eval)
+		df = df.join(pd.json_normalize(df['endpoint_request']))
+		df.drop(columns=['endpoint_request'], inplace=True)
+
 		df = df.dropna(axis='columns',how='all')
-
-		if 'endpoint_request' in df.columns:
-			#df['endpoint_request'] = df['endpoint_request'].to_json(orient="records")
-			df['endpoint_request']= df['endpoint_request'].fillna({i: {} for i in df.index})
-			#df['endpoint_request'] = df['endpoint_request'].apply(ast.literal_eval)
-			df = df.join(pd.json_normalize(df['endpoint_request']))
-			df.drop(columns=['endpoint_request'], inplace=True)
-
 		cols = df.columns.tolist()
 		#df.set_index(cols, inplace=True)
 		#df = df.sort_index()
