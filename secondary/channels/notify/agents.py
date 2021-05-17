95e6a1aefb294d1bdd55cc14c5dfaf7cb93f7888
root <root@localhost.localdomain>
2021-05-18 01:58:32 +0300

    Updates

--- secondary/channels/notify/agents.py
+++ secondary/channels/notify/agents.py
@@ -115,7 +115,13 @@ async def send_outbound_message(messages):
 		lgr.info(f'3:Elapsed {elapsed()}')
 		lgr.info('DF: %s' % df)
 		df['batch'] = pd.to_numeric(df['batch'])
-		df['endpoint_request'] = df['endpoint_request'].to_json(orient="records")
+
+		#df['endpoint_request'] = df['endpoint_request'].to_json(orient="records")
+		df['endpoint_request']= df['endpoint_request'].fillna({i: {} for i in df.index})
+		#df['endpoint_request'] = df['endpoint_request'].apply(ast.literal_eval)
+		df = df.join(pd.json_normalize(df['endpoint_request']))
+		df.drop(columns=['endpoint_request'], inplace=True)
+
 		df = df.dropna(axis='columns',how='all')
 		cols = df.columns.tolist()
 		#df.set_index(cols, inplace=True)
