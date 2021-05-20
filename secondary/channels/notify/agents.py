89c523ce1e5bd3e7670969233cb9ad8e82c3572c
root <root@localhost.localdomain>
2021-05-20 16:31:41 +0300

    Updates

--- secondary/channels/notify/agents.py
+++ secondary/channels/notify/agents.py
@@ -256,14 +256,18 @@ async def send_outbound_messages(is_bulk=True, limit_batch=100):
 	except Exception as e: lgr.error(f'Send Outbound Messages Error: {e}')
 
 
-@app.timer(interval=1)
+#@app.timer(interval=1)
+@app.task()
 async def _nbulk_send_outbound_messages(app):
 	try:
 		await send_outbound_messages(is_bulk=False, limit_batch=60)
+		await asyncio.sleep(1)
 	except Exception as e: lgr.error(f'Non-Bulk Send Outbound Messages Error: {e}')
 
-@app.timer(interval=1)
+#@app.timer(interval=1)
+@app.task()
 async def _bulk_send_outbound_messages(app):
 	try:
 		await send_outbound_messages(is_bulk=True, limit_batch=240)
+		await asyncio.sleep(1)
 	except Exception as e: lgr.error(f'Bulk Send Outbound Messages Error: {e}')
