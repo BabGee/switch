83861378dba7691dc081510bda4dbaf2d7ecb384
root <root@localhost.localdomain>
2021-05-22 18:11:25 +0300

    Updates

--- secondary/channels/notify/tasks.py
+++ secondary/channels/notify/tasks.py
@@ -2115,7 +2115,7 @@ def _send_outbound_messages(is_bulk=True, limit_batch=100):
 		with transaction.atomic():
 			#.order_by('contact__product__priority').select_related('contact','template','state').all
 			#|Q(state__name="PROCESSING",date_modified__lte=timezone.now()-timezone.timedelta(minutes=20),date_created__gte=timezone.now()-timezone.timedelta(minutes=60))\
-			orig_outbound = Outbound.objects.select_for_update(of=('self',)).filter(Q(contact__subscribed=True),Q(contact__product__notification__code__channel__name='SMS'),~Q(recipient=None),
+			orig_outbound = Outbound.objects.select_for_update(of=('self',)).filter(Q(contact__subscribed=True),Q(contact__product__notification__code__channel__name='WHATSAPP API'),~Q(recipient=None),
 					Q(contact__status__name='ACTIVE',contact__product__is_bulk=is_bulk),
 					Q(Q(contact__product__trading_box=None)|Q(contact__product__trading_box__open_time__lte=timezone.localtime().time(),
 					contact__product__trading_box__close_time__gte=timezone.localtime().time())),
