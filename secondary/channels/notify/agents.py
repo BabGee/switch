f1467cfeb43a726afa71c9d52f6d07d1bb1bf28d
root <root@localhost.localdomain>
2021-05-23 19:13:35 +0300

    Updates

--- secondary/channels/notify/agents.py
+++ secondary/channels/notify/agents.py
@@ -68,12 +68,12 @@ async def sent_messages(messages):
 						outbound.batch_id = batch_id
 						__outbound_list.append(outbound)
 					except ObjectDoesNotExist: pass
-					yield __outbound_list
+				return __outbound_list
 
 			outbound_list = await app.loop.run_in_executor(thread_pool, _outbound_list, df)
 
 			lgr.info(f'{elapsed()} Sent Messages Outbound List {outbound_list}')
-			await sync_to_async(Outbound.objects.bulk_update, thread_sensitive=True)([*outbound_list], ['state','response','batch_id'])
+			await sync_to_async(Outbound.objects.bulk_update, thread_sensitive=True)(outbound_list, ['state','response','batch_id'])
 			lgr.info(f'{elapsed()} Sent Messages Updated')
 			await asyncio.sleep(2.0)
 		except Exception as e: lgr.info(f'Error on Sent Messages: {e}')
@@ -107,11 +107,11 @@ async def delivery_status(messages):
 							response=response_code)
 						lgr.info(f'{elapsed()} Multi Delivery Status Outbound {_outbound}')
 					except ObjectDoesNotExist: pass
-					yield __outbound_list
+				return __outbound_list
 
 			outbound_list = await app.loop.run_in_executor(thread_pool, _outbound_list, df)
 			lgr.info(f'{elapsed()} Delivery Status Outbound List {outbound_list}')
-			await sync_to_async(Outbound.objects.bulk_update, thread_sensitive=True)([*outbound_list], ['state','response'])
+			if outbound_list: await sync_to_async(Outbound.objects.bulk_update, thread_sensitive=True)(outbound_list, ['state','response'])
 			lgr.info(f'{elapsed()} Delivery Status Updated')
 			await asyncio.sleep(15.0)
 		except Exception as e: lgr.info(f'Error on Delivery Status: {e}')
