a27401e3817976569d3441eedc4c92474ca6cd07
root <root@localhost.localdomain>
2021-05-23 11:36:53 +0300

    Updates

---
+++ secondary/channels/notify/commands.py
@@ -0,0 +1,50 @@
+import faust
+from faust.types import StreamT
+#from primary.core.async.faust import app
+from switch.faust import app
+import requests, json, ast
+
+
+from django.db import transaction
+from .models import *
+from django.db.models import Q,F
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from functools import reduce
+from concurrent.futures import ThreadPoolExecutor
+from switch.kafka import app as kafka_producer
+from mode import Service
+
+from itertools import islice, chain
+import pandas as pd
+import numpy as np
+import time
+from asgiref.sync import sync_to_async
+import asyncio
+import random
+import logging
+import aiohttp
+from http.client import HTTPConnection  # py3
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    FrozenSet,
+    List,
+    Mapping,
+    MutableMapping,
+    Set,
+    Tuple,
+    Type,
+    cast,
+)
+
+lgr = logging.getLogger(__name__)
+
+
+
+
+@app.command()
+async def example():
+	"""This docstring is used as the command help in --help."""
+	while 1:
+		print('RUNNING EXAMPLE COMMAND')
