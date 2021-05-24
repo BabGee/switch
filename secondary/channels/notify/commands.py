5afbc3c4c4f44fadb493b0d553e6875f2ef44273
root <root@localhost.localdomain>
2021-05-24 13:03:49 +0300

    Updates

--- secondary/channels/notify/commands.py
+++ secondary/channels/notify/commands.py
@@ -72,7 +72,7 @@ def _send_outbound_messages(is_bulk=True, limit_batch=100):
 			if len(outbound):
 				messages=np.asarray(outbound)
 				lgr.info(f'2:Elapsed {elapsed()}')
-				lgr.info('Messages: %s' % messages)
+				#lgr.info('Messages: %s' % messages)
 
 				##Update State
 				processing = orig_outbound.filter(id__in=messages[:,0].tolist()).update(state=OutBoundState.objects.get(name='PROCESSING'), date_modified=timezone.now(), sends=F('sends')+1)
