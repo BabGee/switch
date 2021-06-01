135b7fc4ba848d7167958eccac8fafb78aa3e858
root <root@localhost.localdomain>
2021-06-01 10:32:02 +0300

    Updates

--- switch/spark.py
+++ switch/spark.py
@@ -1,28 +1,30 @@
-import pyspark
-import socket
+#import pyspark
+#import socket
+#
+#from pyspark.sql import SparkSession
+#
+#sock_name = socket.gethostname()
+#host = socket.gethostbyname(sock_name)
+#
+#conf = pyspark.SparkConf()\
+#        .setAll([
+#            ('spark.driver.host', host),
+#            ("spark.cassandra.connection.host", "cassandra-0-service"),
+#            ])
+#
+#
+##.config("spark.jars.packages", 
+##        "")\
+##"org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.1,io.delta:delta-core_2.12:1.0.0")\
+##                .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")\
+##                .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
+#
+#app = SparkSession.builder\
+#                .master("spark://spark-master:7077")\
+#                .config("spark.jars.packages", 
+#                        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.1")\
+#                .config(conf=conf)\
+#                .appName('switch-faust')\
+#                .getOrCreate()
 
-from pyspark.sql import SparkSession
-
-sock_name = socket.gethostname()
-host = socket.gethostbyname(sock_name)
-
-conf = pyspark.SparkConf()\
-        .setAll([
-            ('spark.driver.host', host),
-            ("spark.cassandra.connection.host", "cassandra-0-service"),
-            ])
-
-
-#.config("spark.jars.packages", 
-#        "")\
-#"org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.1,io.delta:delta-core_2.12:1.0.0")\
-#                .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")\
-#                .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
-
-app = SparkSession.builder\
-                .master("spark://spark-master:7077")\
-                .config("spark.jars.packages", 
-                        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.1")\
-                .config(conf=conf)\
-                .appName('switch-faust')\
-                .getOrCreate()
+app = None
