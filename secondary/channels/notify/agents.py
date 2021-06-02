10737aa96c50452037a9f347fe0afe484081559d
root <root@localhost.localdomain>
2021-06-02 22:32:25 +0300

    Updates

--- secondary/channels/notify/agents.py
+++ secondary/channels/notify/agents.py
@@ -58,7 +58,7 @@ async def sent_messages(messages):
 			elapsed = lambda: time.perf_counter() - s
 			lgr.info(f'RECEIVED Sent Message {message}')
 
-			query = """INSERT INTO outbound_notification (product_id, outbound_id, batch_id, channel, 
+			query = """INSERT INTO notify.outbound_notification (product_id, outbound_id, batch_id, channel, 
 				code, date_created, date_modified, message, mno, recipient, response, state) 
 				VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
 				
