892b530a9222a649a3c6db982054cc0aef0e3c82
root <root@localhost.localdomain>
2021-06-03 06:19:47 +0300

    Updates

--- switch/spark.py
+++ switch/spark.py
@@ -1,39 +1,32 @@
-import pyspark
-import socket
-
-from pyspark.sql import SparkSession
-
-sock_name = socket.gethostname()
-host = socket.gethostbyname(sock_name)
-
-conf = pyspark.SparkConf()\
-        .setAll([
-            ('spark.executor.memory', '1g'), 
-            ('spark.executor.cores', '1'), 
-            ('spark.driver.host', host),
-            ("spark.shuffle.service.enabled", "false"),
-            ("spark.dynamicAllocation.enabled", "false"),
-            ('spark.cores.max', '1'), 
-            ('spark.driver.memory','1g'),
-            ("spark.cassandra.connection.host", "cassandra-0-service"),
-            ])
-
-
-#.config("spark.jars.packages", 
-#        "")\
-#"org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.1,io.delta:delta-core_2.12:1.0.0")\
-#                .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")\
-#                .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
-
-app = SparkSession.builder\
-                .master("spark://spark-master:7077")\
-                .config("spark.jars.packages", 
-                        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.1")\
-                .config(conf=conf)\
-		.config("spark.ui.port", 4040) \
-                .appName('switch-spark')\
-                .getOrCreate()
+#import pyspark
+#import socket
+#
+#from pyspark.sql import SparkSession
+#
+#sock_name = socket.gethostname()
+#host = socket.gethostbyname(sock_name)
+#
+#conf = pyspark.SparkConf()\
+#        .setAll([
+#            ('spark.executor.memory', '1g'), 
+#            ('spark.executor.cores', '1'), 
+#            ('spark.driver.host', host),
+#            ("spark.shuffle.service.enabled", "false"),
+#            ("spark.dynamicAllocation.enabled", "false"),
+#            ('spark.cores.max', '1'), 
+#            ('spark.driver.memory','1g'),
+#            ("spark.cassandra.connection.host", "cassandra-0-service"),
+#            ])
+#
+#app = SparkSession.builder\
+#                .master("spark://spark-master:7077")\
+#                .config("spark.jars.packages", 
+#                        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1,com.datastax.spark:spark-cassandra-connector_2.12:3.0.1")\
+#                .config(conf=conf)\
+#		.config("spark.ui.port", 4040) \
+#                .appName('switch-spark')\
+#                .getOrCreate()
+#
 
 
-
-#app = None
+app = None
