bd7c51fdaf4b5b4e1132da9c5068ccae149bc305
root <root@localhost.localdomain>
2021-06-29 06:55:52 +0300

    Updates

--- switch/cassandra.py
+++ switch/cassandra.py
@@ -1,15 +1,16 @@
-from cassandra.cqlengine import connection
-from cassandra.cqlengine.connection import session
-from cassandra.cqlengine import connection
-from cassandra.cqlengine.connection import (
-    cluster as cql_cluster, session as cql_session)
+try:
+	from cassandra.cqlengine import connection
+	from cassandra.cqlengine.connection import session
+	from cassandra.cqlengine import connection
+	from cassandra.cqlengine.connection import (
+	    cluster as cql_cluster, session as cql_session)
 
-def cassandra_init(**kwargs):
-	""" Initialize a clean Cassandra connection. """
-	if cql_cluster is not None:
-		cql_cluster.shutdown()
-	if cql_session is not None:
-		cql_session.shutdown()
-	#connection.setup()
-	connection.setup(['cassandra-0-service'], "switch")
-
+	def cassandra_init(**kwargs):
+		""" Initialize a clean Cassandra connection. """
+		if cql_cluster is not None:
+			cql_cluster.shutdown()
+		if cql_session is not None:
+			cql_session.shutdown()
+		#connection.setup()
+		connection.setup(['cassandra-0-service'], "switch")
+except Exception as e: print(f'Cassandra Error {e}')
