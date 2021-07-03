b1b527cfa0feef0722af9115a04eb4f9b1a50924
root <root@localhost.localdomain>
2021-07-03 14:06:19 +0300

    Updates

--- secondary/channels/notify/models.py
+++ secondary/channels/notify/models.py
@@ -271,12 +271,21 @@ class Outbound(models.Model):
 	def attachment_list(self):
 		return "\n".join([a.name for a in self.attachment.all()])
 
+class SessionSubscriptionStatus(models.Model):
+	date_modified  = models.DateTimeField(auto_now=True)
+	date_created = models.DateTimeField(auto_now_add=True)
+	name = models.CharField(max_length=45, unique=True)
+	description = models.CharField(max_length=100)
+	def __str__(self):
+		return u'%s' % (self.name)
+
 class SessionSubscription(models.Model):
 	date_modified  = models.DateTimeField(auto_now=True)
 	date_created = models.DateTimeField(auto_now_add=True)
 	gateway_profile = models.ForeignKey(GatewayProfile, on_delete=models.CASCADE)
 	enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 	last_access = models.DateTimeField()
+	status = models.ForeignKey(SessionSubscriptionStatus, on_delete=models.CASCADE)
 	def __str__(self):
 		return u'%s' % (self.gateway_profile)
 
