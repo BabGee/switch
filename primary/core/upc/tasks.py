d63075a585f415b2c8d4377bdaa17fc5b0677a09
BabGee <jeremiah.omondi@interintel.co.ke>
2021-11-26 02:35:47 +0000

    changes

--- primary/core/upc/tasks.py
+++ primary/core/upc/tasks.py
@@ -2907,7 +2907,7 @@ class System(Wrappers):
 				payload['response'] = 'Session Profile Captured'               
 			else:
 				payload = self.create_user_profile(payload, node_info)
-				payload['trigger'] = 'no_profile%s' % (',' + payload['trigger'] if 'trigger' in payload.keys() else '')               
+				payload['trigger'] = 'not_registered%s' % (',' + payload['trigger'] if 'trigger' in payload.keys() else '')               
 				if 'response_status' in payload.keys() and payload['response_status'] == '00':
 					payload['response'] = 'Session Profile Captured'                   
 		except Exception as e:
