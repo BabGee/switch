ee0bdab38930ce4b7f4e335dccfaadd87f595b15
samsonarita <samson.arita@gmail.com>
2021-12-14 07:12:02 +0300

    Update

--- secondary/channels/dsc/commands.py
+++ secondary/channels/dsc/commands.py
@@ -44,6 +44,42 @@ from typing import (
 )
 
 lgr = logging.getLogger(__name__)
+thread_pool = ThreadPoolExecutor(max_workers=16)
+
+def process_file_upload(activity_id, status):
+	try:
+		s = time.perf_counter()
+		elapsed = lambda: time.perf_counter() - s
+
+		u = FileUploadActivity.objects.get(id=activity_id)
+
+		rf = u.file_path
+		df = pd.read_csv(rf)
+
+		lgr.info('Data Frame Columns: %s' % df.columns)
+		lgr.info('Data Frame: \n%s' % df.head())
+		lgr.info('Data Frame: \n%s' % df.tail())
+
+		columns = [c.strip().lower().replace(' ','_')	for c in df.columns]
+
+		lgr.info('Data Frame Columns: %s' % columns)
+
+		service = u.file_upload.activity_service
+		gateway_profile = u.gateway_profile
+
+		for r in zip(*df.to_dict("list").values()):
+			payload = dict(zip(columns, r))
+
+			lgr.info(f'1: Elapsed {elapsed()} File Upload  - {payload}')
+			response = BridgeWrappers().background_service_call(service, gateway_profile, payload)
+
+			lgr.info(f'2: Elapsed {elapsed()} File Upload - {response}')
+
+		u.status=FileUploadActivityStatus.objects.get(name='PROCESSED')
+		u.save()
+
+	except Exception as e: lgr.info('Unable to process file upload: %s' % e)
+
 
 @_faust.command()
 async def dsc_file_upload():
@@ -58,17 +94,20 @@ async def dsc_file_upload():
 			s = time.perf_counter()
 			elapsed = lambda: time.perf_counter() - s
 			tasks = list()
+			activity = list()
 			with transaction.atomic():
 				lgr.info(f'1:File Upload-Elapsed {elapsed()}')
 				orig_file_upload = await sync_to_async(upload_query, thread_sensitive=True)(status='CREATED')
 				lgr.info(f'{elapsed()}-Orig File Upload: {orig_file_upload}')
-				for f in orig_file_upload:
+				activity = list(orig_background.values_list('id',flat=True)[:5])
+
+				processing = orig_file_upload.filter(id__in=activity).update(status=FileUploadActivityStatus.objects.get(name='PROCESSING'), date_modified=timezone.now())
+
+				for a in activity:
 				    lgr.info(f'File Upload: {f}')
-				    fu = sync_to_async(BridgeWrappers().background_service_call, thread_sensitive=True)(f.file_upload.activity_service, f.gateway_profile, f.details)
+				    fu = _faust.loop.run_in_executor(thread_pool, process_file_upload, *[a, True])
 
 				    tasks.append(fu)
-				#Mark Activities as processed
-				orig_file_upload.update(status=FileUploadActivityStatus.objects.get(name='PROCESSED'))
 			#End Atomic Transaction
 			lgr.info(f'2:File Upload-Elapsed {elapsed()}')
 			if tasks: response = await asyncio.gather(*tasks)
