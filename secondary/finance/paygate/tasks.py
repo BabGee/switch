5d353ab923a6cf117e2c7e169ae1e45f4cf23ed0
samsonarita <samson.arita@gmail.com>
2021-12-30 01:28:49 +0300

    Update

--- secondary/finance/paygate/tasks.py
+++ secondary/finance/paygate/tasks.py
@@ -404,10 +404,8 @@ class System(Wrappers):
 					retry = False
 					try:
 
-
 						#Avoid Race condition in transactions inserts to ensure unique entries for ext_inbound_id
-						last_incoming  = Incoming.objects.select_for_update().filter(reference=reference, remittance_product=product, 
-											date_created__gte=timezone.now()-timezone.timedelta(hours=24) ).order_by('-id')
+						last_incoming  = Incoming.objects.select_for_update().filter(reference=reference, remittance_product=product).order_by('-id')
 						if len(last_incoming): last_incoming.filter(id=last_incoming.first().id).update(updated=True)
 
 						lgr.info('Past Race Condition Check')
