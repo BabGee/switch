52384cdb55bc618d304b41948ca4f8763a8ed584
samsonarita <samson.arita@gmail.com>
2021-12-30 07:16:31 +0300

    Update

--- primary/core/api/agents.py
+++ primary/core/api/agents.py
@@ -159,7 +159,7 @@ def api_service_call(payload):
         else:
                 lgr.info(f'Gateway Profile Does not Exist')
 
-        return payload
+        #return payload
 
 
 @_faust.agent(service_topic, concurrency=4)
